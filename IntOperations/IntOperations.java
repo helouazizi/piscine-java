3510d551c0e091f3093f47f5732cb61c0b5451bb
helouazizi <ouazizi2code@gmail.com>
2025-11-03 11:20:02 +0100

    add int operations

---
+++ IntOperations/IntOperations.java
@@ -0,0 +1,22 @@
+public class IntOperations {
+    public static int addTwoIntegers(int a, int b) {
+
+        return a + b;
+    }
+
+    public static int subtractTwoIntegers(int a, int b) {
+
+        return a - b;
+    }
+
+    public static int multiplyTwoIntegers(int a, int b) {
+
+        return a * b;
+    }
+
+    public static int divideTwoIntegers(int a, int b) {
+
+        return a / b;
+    }
+
+}
