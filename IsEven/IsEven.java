e47618aca0ab874ae73c6bb1572c4125fe9ac04b
helouazizi <ouazizi2code@gmail.com>
2025-11-03 12:10:38 +0100

    ad is even

---
+++ IsEven/IsEven.java
@@ -0,0 +1,10 @@
+public class IsEven {
+    public static boolean isEven(int a) {
+        // your code here
+        if (a % 2 == 0) {
+            return true ;
+        }
+        return false ;
+    }
+}
+
