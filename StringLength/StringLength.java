b5093958ec39c58db7f10f8df3793e1528abdd28
helouazizi <ouazizi2code@gmail.com>
2025-11-03 13:39:44 +0100

    add ciondition

--- StringLength/StringLength.java
+++ StringLength/StringLength.java
@@ -1,6 +1,9 @@
 public class StringLength {
     public static int getStringLength(String s) {
         // your code here
+        if (s == null) {
+            return 0 ;
+        }
         return s.length() ;
     }
 }
