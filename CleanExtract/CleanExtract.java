e7d603afeaded8957bc26ec24fdc63211ed4ea5e
helouazizi <ouazizi2code@gmail.com>
2025-11-03 16:38:50 +0100

    add trim

--- CleanExtract/CleanExtract.java
+++ CleanExtract/CleanExtract.java
@@ -1,6 +1,19 @@
 public class CleanExtract {
     public static String extract(String s) {
-        // your code here
-        return s;
+        String res = "";
+        var parst = s.split("\\|");
+        for (String part : parst) {
+            int first = part.indexOf(".");
+            int last = part.lastIndexOf(".");
+            if (first >= 0 && last >= 0 && last > first) {
+                res += part.substring(first + 1, last);
+            } else if (first == last) {
+                res += part.substring(first + 1);
+            } else if (first == -1 && last == -1 && !part.isEmpty()) {
+                res += part.trim();
+            }
+
+        }
+        return res.replaceAll("\\s+", " ").trim();
     }
 }
