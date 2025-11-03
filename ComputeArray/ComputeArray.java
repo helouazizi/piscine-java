4909b767d57264f964de8e8178ae232a33be542c
helouazizi <ouazizi2code@gmail.com>
2025-11-03 16:58:29 +0100

    zzzzzz

--- ComputeArray/ComputeArray.java
+++ ComputeArray/ComputeArray.java
@@ -1,10 +1,13 @@
 public class ComputeArray {
     public static int[] computeArray(int[] array) {
+        if (array  == null) {
+            return null ;
+        }
         int[] result = new int[array.length];
 
         for (int i = 0; i < array.length; i++) {
             int num = array[i];
-            int remainder = ((num % 3) + 3) % 3;
+            int remainder = (num % 3) ;
 
             if (remainder == 0) {
                 result[i] = num * 5;
