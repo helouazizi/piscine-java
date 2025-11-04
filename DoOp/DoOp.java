e9e687e2dca252203aa5f1781e7b36b82559f9e4
helouazizi <ouazizi2code@gmail.com>
2025-11-04 10:32:52 +0100

    zzzzzzzzz

--- DoOp/DoOp.java
+++ DoOp/DoOp.java
@@ -1,34 +1,31 @@
 public class DoOp {
     public static String operate(String[] args) {
-        // your code here
         if (args.length == 0) {
-            return new String("it depend on the input.");
+            return "it depend on the input.";
         }
+
         int first = Integer.parseInt(args[0]);
         int second = Integer.parseInt(args[2]);
 
         switch (args[1]) {
             case "+":
-                return (first + second) + "";
+                return String.valueOf(first + second);
             case "-":
-                return (first - second) + "";
+                return String.valueOf(first - second);
             case "*":
-
-                return (first * second) + "";
+                return String.valueOf(first * second);
             case "/":
-                if (second <= 0) {
+                if (second == 0) {
                     return "Error";
                 }
-                return (first / second) + "";
+                return String.valueOf(first / second);
             case "%":
-                if (second <= 0) {
+                if (second == 0) {
                     return "Error";
                 }
-                return (first % second) + "";
-
+                return String.valueOf(first % second);
             default:
-                return "it depend on the input.";
+                return "It depends on the input.";
         }
-
     }
 }
