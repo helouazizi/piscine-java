4d84ea2af80fe514fe13415a2c6396653ed39deb
helouazizi <ouazizi2code@gmail.com>
2025-11-04 14:06:27 +0100

    ad search file

---
+++ FileSearch/FileSearch.java
@@ -0,0 +1,35 @@
+import java.io.File;
+
+public class FileSearch {
+    public static String searchFile(String fileName) {
+        File directory = new File("documents");
+        return searchInDirectory(directory, fileName);
+    }
+
+    private static String searchInDirectory(File directory, String fileName) {
+        if (directory == null || !directory.exists() || !directory.isDirectory()) {
+            return null;
+        }
+
+        File[] files = directory.listFiles();
+
+        if (files == null) {
+            return null;
+        }
+
+        for (File file : files) {
+            if (file.isFile() && file.getName().equals(fileName)) {
+                return file.getAbsolutePath();
+            }
+
+            if (file.isDirectory()) {
+                String result = searchInDirectory(file, fileName);
+                if (result != null) {
+                    return result;
+                }
+            }
+        }
+
+        return null;
+    }
+}
