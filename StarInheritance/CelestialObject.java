f87e351f6c4554ad4a0708dd0cce1cf858a5cda4
helouazizi <ouazizi2code@gmail.com>
2025-11-05 12:11:25 +0100

    add StarUtils

--- StarInheritance/CelestialObject.java
+++ StarInheritance/CelestialObject.java
@@ -1,6 +1,9 @@
+import java.util.Objects;
+
 public class CelestialObject {
     public double x, y, z;
     public String name;
+    public static double KM_IN_ONE_AU = 150000000;
 
     // Default constructor
     public CelestialObject() {
@@ -49,6 +52,34 @@ public class CelestialObject {
         this.name = name;
     }
 
-  
+    public static double getDistanceBetween(CelestialObject defeoult, CelestialObject obj2) {
+        double dx = obj2.x - defeoult.x;
+        double dy = obj2.y - defeoult.y;
+        double dz = obj2.z - defeoult.z;
+
+        return Math.sqrt(dx * dx + dy * dy + dz * dz);
+    }
+
+    public static double getDistanceBetweenInKm(CelestialObject defeoult, CelestialObject obj2) {
+        double d = getDistanceBetween(defeoult, obj2);
+
+        return d * KM_IN_ONE_AU;
+    }
+
+    public String toString() {
+        return String.format("%s is positioned at (%.3f, %.3f, %.3f)", this.name, this.x, this.y, this.z);
+    }
+
+    public boolean equals(CelestialObject obj) {
+        if (this == obj)
+            return true; 
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        return this.name == obj.name && this.x == obj.x && this.y == obj.y && this.z == obj.z;
+    }
+
+    public int hashCode() {
+        return Objects.hash(name, x, y, z);
+    }
 
 }
