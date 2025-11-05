5e622788274d301f0d69f8c2ebbe6b995d9c62ae
helouazizi <ouazizi2code@gmail.com>
2025-11-05 10:32:56 +0100

    add getters

---
+++ StarGetters/CelestialObject.java
@@ -0,0 +1,56 @@
+public class CelestialObject {
+    public double x, y, z;
+    public String name;
+
+    // Default constructor
+    public CelestialObject() {
+        this.x = 0.0;
+        this.y = 0.0;
+        this.z = 0.0;
+        this.name = "Soleil";
+    }
+
+    public CelestialObject(String name, double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.name = name;
+    }
+
+    // System.out.println(defaultStar.getX());
+    // System.out.println(defaultStar.getY());
+    // System.out.println(defaultStar.getZ());
+    // System.out.println(defaultStar.getName());
+    public double getX() {
+        return this.x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+    public double getZ() {
+        return this.z;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void  setX( double x) {
+        this.x = x ;
+    }
+
+    public void  setY(double y) {
+         this.y = y;
+    }
+
+    public void setZ(double z) {
+         this.z = z;
+    }
+
+    public void  setName(String name ) {
+         this.name = name;
+    }
+
+}
