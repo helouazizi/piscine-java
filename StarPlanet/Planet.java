131ef4eb46cab7612c1b8f84ac8e767623110847
helouazizi <ouazizi2code@gmail.com>
2025-11-05 14:34:50 +0100

    foramt

--- StarPlanet/Planet.java
+++ StarPlanet/Planet.java
@@ -4,50 +4,42 @@ public class Planet extends CelestialObject {
 
     private Star centerStar;
 
-    // Default constructor
     public Planet() {
-        super(); // Call CelestialObject default constructor
-        this.centerStar = new Star(); // Initialize centerStar with default Star
+        super();
+        this.centerStar = new Star();
     }
 
-    // Parameterized constructor
     public Planet(String name, double x, double y, double z, Star centerStar) {
-        super(name, x, y, z); // Call CelestialObject parameterized constructor
+        super(name, x, y, z);
         this.centerStar = centerStar;
     }
 
-    // Getter
     public Star getCenterStar() {
         return centerStar;
     }
 
-    // Setter
     public void setCenterStar(Star centerStar) {
         this.centerStar = centerStar;
     }
 
-    // Equals based on centerStar only
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
             return true;
         if (obj == null || getClass() != obj.getClass())
             return false;
-        if (!super.equals(obj))
-            return false;
         Planet other = (Planet) obj;
-        return Objects.equals(centerStar, other.centerStar);
+        return this.name.equals(other.name) && this.x == other.x && this.y == other.y && this.z == other.z && this.centerStar.equals(other.centerStar);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), centerStar);
+        return Objects.hash(name,x,y,z,centerStar);
     }
 
-    // toString with formatted distance
     @Override
     public String toString() {
-        double distanceWithCenterStar = CelestialObject.getDistanceBetween(this, centerStar);
+        double distanceWithCenterStar = getDistanceBetween(this, centerStar);
         return String.format("%s circles around %s at the %.3f AU",
                 getName(),
                 centerStar.getName(),
