54f9dcbd57a7afa3aeeb60593b530348cc18509e
helouaziz <ouazizi2code@gmail.com>
2025-11-10 15:51:36 +0100

    add .

---
+++ AdventureInterface/ExerciseRunner.java
@@ -0,0 +1,8 @@
+public class ExerciseRunner {
+    public static void main(String[] args) {
+        System.out.println("Running exercise for Tank...");
+        // You can instantiate and test your Tank class here
+        // Example:
+        // Tank obj = new Tank();
+    }
+}
