9868e3061cf34439c05ee542be00b15e9f9b9f3b
helouazizi <ouazizi2code@gmail.com>
2025-11-11 12:01:26 +0100

    add

---
+++ AdventureException/Monster.java
@@ -0,0 +1,37 @@
+public class Monster extends Character {
+    // public Monster(String name, int maxHealth) {
+    // super(name, maxHealth);
+    // }
+
+    public Monster(String name, int maxHealth, Weapon massue) {
+        super(name, maxHealth, massue);
+    }
+
+    public String toString() {
+
+        return this.getCurrentHealth() > 0
+                ? String.format("%s is a monster with %d HP", this.getName(), this.getCurrentHealth())
+                : String.format("%s is a monster and is dead", this.getName());
+    }
+
+    @Override
+    public void attack(Character c) {
+        Weapon weapon = this.getWeapon();
+        int damage = (weapon != null) ? weapon.getDamage() :7;
+        c.takeDamage(damage);
+    }
+
+    @Override
+    public void takeDamage(int i) {
+        int reducedDamage = (int) Math.floor(i * 0.8);
+
+        int newHealth = this.getCurrentHealth() - reducedDamage;
+
+        if (newHealth < 0) {
+            newHealth = 0;
+        }
+
+        this.setCurrentHealth(newHealth);
+    }
+
+}
