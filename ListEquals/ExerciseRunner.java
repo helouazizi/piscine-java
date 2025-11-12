0c86e86fbd1f9e8a24ba1354b28cd148999d9175
helouazizi <ouazizi2code@gmail.com>
2025-11-12 10:24:13 +0100

    add equals

---
+++ ListEquals/ExerciseRunner.java
@@ -0,0 +1,8 @@
+public class ExerciseRunner {
+    public static void main(String[] args) {
+        System.out.println("Running exercise for ListEquals...");
+        // You can instantiate and test your ListEquals class here
+        // Example:
+        // ListEquals obj = new ListEquals();
+    }
+}
