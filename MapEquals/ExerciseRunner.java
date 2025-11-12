1bce444b5be9e5e2e9508d980388de5a45238304
helouazizi <ouazizi2code@gmail.com>
2025-11-12 11:32:36 +0100

    Add MapEquals class with method to compare two maps for equality

---
+++ MapEquals/ExerciseRunner.java
@@ -0,0 +1,8 @@
+public class ExerciseRunner {
+    public static void main(String[] args) {
+        System.out.println("Running exercise for MapEquals...");
+        // You can instantiate and test your MapEquals class here
+        // Example:
+        // MapEquals obj = new MapEquals();
+    }
+}
