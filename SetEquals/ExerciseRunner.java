4dd548d7152ba3897aa9ce329b9f44fdbf78f9c0
helouazizi <ouazizi2code@gmail.com>
2025-11-12 11:39:03 +0100

    Add SetEquals class with method to compare two sets for equality and create ExerciseRunner for testing

---
+++ SetEquals/ExerciseRunner.java
@@ -0,0 +1,8 @@
+public class ExerciseRunner {
+    public static void main(String[] args) {
+        System.out.println("Running exercise for SetEquals...");
+        // You can instantiate and test your SetEquals class here
+        // Example:
+        // SetEquals obj = new SetEquals();
+    }
+}
