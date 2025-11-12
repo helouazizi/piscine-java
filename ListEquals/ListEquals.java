1102360ac0c21cabcce10055d441ed6a90c77018
helouazizi <ouazizi2code@gmail.com>
2025-11-12 10:25:28 +0100

    add conditions

--- ListEquals/ListEquals.java
+++ ListEquals/ListEquals.java
@@ -1,6 +1,9 @@
 import java.util.List;
 public class ListEquals {
     public static boolean areListsEqual(List<String> list1, List<String> list2) {
+        if (list1 == null || list1 == null) {
+            return false ;
+        }
         // your code here
         return list1.equals(list2);
     }
