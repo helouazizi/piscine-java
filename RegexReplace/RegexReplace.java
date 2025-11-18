998fa2300e8f5374c57fed998e4de514be484df8
helouazizi <ouazizi2code@gmail.com>
2025-11-18 10:00:48 +0100

    add some fixes

--- RegexReplace/RegexReplace.java
+++ RegexReplace/RegexReplace.java
@@ -1,9 +1,60 @@
+
 public class RegexReplace {
+
     public static String removeUnits(String s) {
-        // your code here
+        return s.replaceAll("(\\d+)(cm|€)", "$1");
     }
-    
+
+
     public static String obfuscateEmail(String s) {
-        // your code here
+
+        String[] parts = s.split("@");
+        String username = parts[0];
+        String domain = parts[1];
+
+
+        if (username.matches(".*[._-].*")) {
+
+            int idx = username.indexOf('.');
+            int dash = username.indexOf('-');
+            int under = username.indexOf('_');
+
+            if (dash != -1 && (idx == -1 || dash < idx)) idx = dash;
+            if (under != -1 && (idx == -1 || under < idx)) idx = under;
+
+            String prefix = username.substring(0, idx + 1);
+
+            String mask = username.substring(idx + 1).replaceAll(".", "*");
+
+            username = prefix + mask;
+
+        } else {
+            if (username.length() > 3) {
+                username = username.substring(0, 3)
+                        + username.substring(3).replaceAll(".", "*");
+            }
+        }
+
+
+        String[] labels = domain.split("\\.");
+
+        if (labels.length == 3) {
+            labels[0] = labels[0].replaceAll(".", "*"); 
+            labels[2] = labels[2].replaceAll(".", "*"); 
+        } else {
+          
+            labels[0] = labels[0].replaceAll(".", "*"); 
+            if (!labels[1].matches("com|org|net")) {
+                labels[1] = labels[1].replaceAll(".", "*"); 
+            }
+        }
+
+        StringBuilder newDomain = new StringBuilder();
+        for (int i = 0; i < labels.length; i++) {
+            newDomain.append(labels[i]);
+            if (i < labels.length - 1) newDomain.append(".");
+        }
+
+        return username + "@" + newDomain.toString();
     }
 }
