1065a6fc8e7121429af278e19f3b4a3136b48e2d
helouazizi <ouazizi2code@gmail.com>
2025-11-18 09:48:04 +0100

    add regex

---
+++ RegexMatch/RegexMatch.java
@@ -0,0 +1,16 @@
+import java.util.regex.Pattern;
+
+public class RegexMatch {
+
+    public static boolean containsOnlyAlpha(String s) {
+        return Pattern.matches("[a-zA-Z]+", s);
+    }
+
+    public static boolean startWithLetterAndEndWithNumber(String s) {
+        return Pattern.matches("^[a-zA-Z].*\\d+$", s);
+    }
+
+    public static boolean containsAtLeast3SuccessiveA(String s) {
+        return Pattern.matches(".*AAA.*", s);
+    }
+}
