d3bc2520443208a72ed703dbf4a42455c819e1b6
helouazizi <ouazizi2code@gmail.com>
2025-11-18 09:49:23 +0100

    add RegexReplace

---
+++ RegexReplace/RegexReplace.java
@@ -0,0 +1,9 @@
+public class RegexReplace {
+    public static String removeUnits(String s) {
+        // your code here
+    }
+    
+    public static String obfuscateEmail(String s) {
+        // your code here
+    }
+}
